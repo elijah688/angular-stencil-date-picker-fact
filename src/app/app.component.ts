7f61f9cc493026e1d69dc67ae65e9c5e47a4c763
elijah688 <elijah.iliev.95@gmail.com>
2019-12-23 12:03:33 +0000

    feat(random fact): user will be supplied with a random fact, via the "numbers API", each time they pick a date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges, OnInit, AfterViewInit, OnChanges } from '@angular/core';
+import { NumbersService } from './numbers.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'stencil-angular-test';
+export class AppComponent implements OnInit {
+
+ 
+  private numSub:Subscription = new Subscription();
+  fact:string;
+  date:Date;
+
+  constructor(private numbServ:NumbersService){
+  }
+
+  ngOnInit(): void {
+    if(this.date){
+      this.numbServ.getDateFact(this.date);
+    }
+    this.numSub = this.numbServ.factSubjact.subscribe(fact=>{
+      this.fact = fact;
+    });
+  }
+
+
+  ngOnDestroy(): void {
+    this.numSub.unsubscribe()    
+  }
+
+  getFact(d:Date):void{
+    const date:Date = (new Date(d['detail']))
+    this.numbServ.getDateFact(date)
+  }
 }
---
+++ src/app/numbers.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Subject, Observable } from 'rxjs';
+import { tap, map} from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NumbersService {
+
+  private _factSubject:Subject<string> = new Subject<string>();
+  constructor(private http:HttpClient) { }
+
+  getDateFact(date:Date):void{
+    const day:number = date.getDate();
+    const month:number = date.getMonth();
+
+    const numbersAPIUrl:string = `${environment.numbersAPI}${month}/${day}/date`
+    
+    this.http.get(numbersAPIUrl, { responseType: 'text' }).subscribe(fact=>{
+      this._factSubject.next(fact)
+    }
+    )
+  }
+
+  get factSubjact():Observable<string>{
+    return this._factSubject.asObservable();
+  }
+}
